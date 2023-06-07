783c5af80aaa2cb2094ca1bc7ad0790352afeaa9
JakubGrezl <jakub.grezl@outlook.cz>
2023-06-07 13:27:35 +0200

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { Container, Row, Col } from '@nextui-org/react';
 import styles from '../styles/page.module.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-
-    </main>
+    <Container xl>
+      <main className={styles.main}>
+        <h1>Tvojemama</h1>
+      </main>
+    </Container>
   )
 }
