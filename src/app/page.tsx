65fc1de77aca445e88963f04713be7b9e6d11210
JakubGrezl <jakub.grezl@outlook.cz>
2023-06-07 14:04:31 +0200

    real

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Container xl>
       <main className={styles.main}>
-        <h1>Tvojemama</h1>
+        <h1>chcispat(real)</h1>
       </main>
     </Container>
   )
