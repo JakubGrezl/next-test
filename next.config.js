a7bb58560f9d72360e4dec03963ebbb2d1d67138
JakubGrezl <jakub.grezl@outlook.cz>
2023-06-09 12:28:34 +0200

    Revert "funguje projects page"
    This reverts commit 9308d80351b281bbd9d17171272b689ac115214c.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,16 @@ const nextConfig = {
     experimental: {
         serverActions: true
     },
+<<<<<<< HEAD
     
     trailingSlash: true,
     output: 'export'
+=======
+
+    images: {
+        formats: ['image/webp'] 
+    }
+>>>>>>> parent of 9308d80 (funguje projects page)
 }
 
 module.exports = nextConfig
